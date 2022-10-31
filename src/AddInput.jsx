4fd96d9e2f173187cf0986305d3fcfe379775d75
viniciusnd <vinicius.santosnd@gmail.com>
2022-10-31 18:05:24 -0300

    Fixing

--- src/AddInput.jsx
+++ src/AddInput.jsx
@@ -2,6 +2,6 @@ import './index.css'
 
 export default function AddInput () {
     return (
-        <input type="text" id="addInput" className="border rounded-xl text-center text-sm uppercase px-0.5 py-1 md:px-10 md:py-6 ml-3 mr-3" placeholder="Add participants" />
+        <input type="text" id="addInput" className="border rounded-xl text-center text-sm uppercase px-0.5 py-1 md:px-10 md:py-6 mr-3" placeholder="Add participants" />
     )
 }
