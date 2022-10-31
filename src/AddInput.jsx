7e7c84526f2474cad25c68b39e2d0bf9cf149f33
viniciusnd <vinicius.santosnd@gmail.com>
2022-10-31 18:31:26 -0300

    adding new dimensions

--- src/AddInput.jsx
+++ src/AddInput.jsx
@@ -2,6 +2,6 @@ import './index.css'
 
 export default function AddInput () {
     return (
-        <input type="text" id="addInput" className="border rounded-xl text-center text-sm uppercase px-0.5 py-1 md:px-10 md:py-6 mr-3" placeholder="Add participants" />
+        <input type="text" id="addInput" className="border rounded-xl text-center text-sm uppercase w-30 md:w-80 h-10 md:h-10 mr-3" placeholder="Add participants" />
     )
 }
--- src/ConfirmPlayInput.jsx
+++ src/ConfirmPlayInput.jsx
@@ -3,6 +3,6 @@ import './index.css'
 
 export default function ConfirmPlayInput() {
     return (
-        <input type="text" id="confirmPlayInput" className="border rounded-xl text-center text-sm uppercase px-0.5 py-1 md:px-10 md:py-6 ml-3" placeholder="Type your name"/>
+        <input type="text" id="confirmPlayInput" className="border rounded-xl text-center text-sm uppercase w-30 md:w-80 h-10 md:h-10 ml-3" placeholder="Type your name"/>
     )
 }
