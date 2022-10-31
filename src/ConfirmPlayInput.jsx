654c1200db20f6e39925b30255003e831bfa301c
viniciusnd <vinicius.santosnd@gmail.com>
2022-10-31 17:50:11 -0300

    fixing

--- src/ConfirmPlayInput.jsx
+++ src/ConfirmPlayInput.jsx
@@ -3,6 +3,6 @@ import './index.css'
 
 export default function ConfirmPlayInput() {
     return (
-        <input type="text" id="confirmPlayInput" className="border rounded-xl text-center text-sm uppercase px-2 py-1 md:px-10 md:py-6 ml-3 mr-3" placeholder="Type your name"/>
+        <input type="text" id="confirmPlayInput" className="border rounded-xl text-center text-sm uppercase px-1 py-1 md:px-10 md:py-6 ml-3 mr-3" placeholder="Type your name"/>
     )
 }
